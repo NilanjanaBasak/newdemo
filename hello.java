7922308c810046774165c3a96bb593d7a2a695fb
NilanjanaBasak <55828082+NilanjanaBasak@users.noreply.github.com>
2019-10-03 19:39:40 +0600

    new branch added and code created

--- hello.java
+++ hello.java
@@ -1,18 +1,14 @@
 public class hello{
 	public static void main(String[] args){
 		System.out.println("Hello java");
-		greeting("Git");
+		
 	}
 }
 
-public static int sqr(int x){
-	return x*x;
-}
 
 
-
-public static void greeting(String name)
+public static void message()
 {
-	System.out.println("Greeting " +name);
+	System.out.println("Greeting git");
 }
 
